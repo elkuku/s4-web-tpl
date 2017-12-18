54cc34ea5d77fe62518955df635390eb8871f912
Nikolai Plath <der.elkuku@gmail.com>
2017-12-18 17:06:43 +0100

    Add a controller

---
+++ src/Controller/Main.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 12.12.17
+ * Time: 08:15
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Main extends Controller
+{
+	/**
+	 * @Route("/", name="welcome")
+	 *
+	 * @return Response
+	 */
+	public function main()
+	{
+        return new Response('Hello =;)');
+	}
+}
