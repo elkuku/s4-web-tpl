7937db51dcc54cb4dae55ba00313009e16670ccd
Nikolai Plath <der.elkuku@gmail.com>
2017-12-18 17:41:12 +0100

    Add node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
+    //.enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
@@ -15,10 +15,20 @@ Encore
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
+
+    .enableVersioning(false)
+
+    // uncomment to define the assets of the project
+    .createSharedEntry('js/common', ['jquery'])
+    .addEntry('js/app', './assets/js/app.js')
+
+    .addStyleEntry('css/app', './assets/css/app.scss')
+
+
 ;
 
 module.exports = Encore.getWebpackConfig();
